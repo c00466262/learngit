40b6288f1152ec3dd55a720268a1cd60c68a7c3c
c00466262 <caowg1990@163.com>
2018-08-19 09:32:31 +0800

    test！！！

--- readme.c
+++ readme.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int func1(void)
+{
+	printf("func1\n");
+	return 1;
+}
+
 int main(int argc, char **argv)
 {
+	func1();
+	printf("learn git\n");
 	printf("learn git\n");
 	return 0;
 }
